22d2772a9e81524d74109ab9aa6f3a27af6a6bee
Santiago Castillo <castillosantiagoe1998@gmail.com>
2022-03-23 00:46:26 -0300

    segunda subida archivo tp1_.c

--- tp1_1.c
+++ tp1_1.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
 int main(){
+    int variablePrueba=3;
+    int *pVariablePrueba;
+    
+    printf("Hola mundo\n");
 
-    printf("Hola mundo");
+    pVariablePrueba = &variablePrueba;
+
+    printf("el contenido del puntero es: %d\n", *pVariablePrueba);
+    printf("La direccion de memoria almacenada por el puntero es %p\n", pVariablePrueba);
+    printf("La direccion de memoria de la variable: %p\n", &variablePrueba);
+    printf("La direccion de memoria del puntero: %p\n", &pVariablePrueba);
+
+    printf("Tamanio de memoria usado por la variable: %d", sizeof(variablePrueba));
+
 
     return 0;
 }
