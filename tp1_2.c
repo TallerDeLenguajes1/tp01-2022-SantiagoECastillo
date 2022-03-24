c03993a780940263e60248ee35b72a0e4bf8915e
Santiago Castillo <castillosantiagoe1998@gmail.com>
2022-03-24 15:59:12 -0300

    terminado de funciones ordenar e invertir

--- tp1_2.c
+++ tp1_2.c
@@ -2,7 +2,7 @@
 
 int cuadradoV1(int numero);
 void cuadradoV2(int *numero);
-void invertir(int a, int b);
+void invertir(int *a, int *b);
 void ordenar(int a, int b);
 
 int main(){
@@ -14,6 +14,14 @@ int main(){
     printf("Primer numero: %d\n", numero1);
     printf("Segundo numero: %d", numero2);
 
+    invertir(&numero1, &numero2);
+    printf("\n\nINVERTIMOS LO VALORES\n");
+    printf("EL primer numero ahora es: %d\n", numero1);
+    printf("EL segundo numero ahora es: %d\n", numero2);
+
+    printf("\nOrdenamos los numeros\n");
+    ordenar(numero1, numero2);
+    
     return 0;
 }
 
@@ -26,6 +34,20 @@ void cuadradoV2(int *numero){
     *numero = (*numero) * (*numero);
 }
 
-void invertir(int a, int b){
-    
+void invertir(int *a, int *b){
+    int auxiliar;
+
+    auxiliar = *a;
+    *a = *b;
+    *b = auxiliar;
+
 }
+
+void ordenar(int a, int b){
+    if(a > b){
+        printf("%d es mayor que %d",a,b);
+    }else{
+        printf("%d es mayor que %d",b,a);
+    }
+}
+
